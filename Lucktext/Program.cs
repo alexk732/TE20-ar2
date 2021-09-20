63136675d6f444c4238a9bc1a01183798694794e
alexk732 <kron.alexander@elev.ga.ntig.se>
2021-09-20 14:45:36 +0200

    Klar med lucktexten

---
+++ Lucktext/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lucktext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vad heter du?");
+            string name = Console.ReadLine();
+
+            Console.Write("Hur många år är du?");
+            string age = Console.ReadLine();
+
+            Console.Write("Vad är din favoritfärg?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine($"Ett rymdskepp som heter {name} åker med hastigheten {age} km/h mot en planet som är helt {color}.");
+
+            //wait before closing
+            Console.ReadKey();
+        }
+    }
+}
