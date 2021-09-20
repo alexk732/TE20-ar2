1e426093f4642a438b05fa01bddf5da928286984
alexk732 <kron.alexander@elev.ga.ntig.se>
2021-09-20 20:34:58 +0200

    Klar med uppgift 1.4

---
+++ Uppgift-1.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift_1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Uppgift 1.4";
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Console.WindowHeight = 30;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("testmeddelande");
+
+            //wait before closing
+            Console.ReadKey();
+        }
+    }
+}
