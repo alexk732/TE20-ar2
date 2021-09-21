784e4f507144cee31a86ea253131100f24baf2f1
alexk732 <kron.alexander@elev.ga.ntig.se>
2021-09-21 11:10:02 +0200

    tränat på datatyper

---
+++ Parse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Parse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            string age = Console.ReadLine();
+
+            Console.WriteLine($"Du är {age} gammal.");
+
+            //Konvertera från string till heltal
+            int ageNumber = int.Parse(age);
+
+            Console.WriteLine($"Det är {20 - ageNumber} år kvar tills du får gå på systembolaget.");
+
+            Console.Write("Hur mycket väger du?");
+            float weight = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Din vikt blir {weight} * 2.2 kg");
+            
+    
+        }
+    }
+}
---
+++ Uppgift-2.13/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift_2._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar vill du hyra bil");
+
+            Console.ReadKey();
+        }
+    }
+}
