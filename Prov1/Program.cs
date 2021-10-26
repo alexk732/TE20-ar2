f06ee9d7e37c02927dc651d34a3586bf33c02c8c
alexk732 <kron.alexander@elev.ga.ntig.se>
2021-10-26 11:02:53 +0200

    Jobbade med Ceasar-kryptot och lite med provet

--- Prov1/Program.cs
+++ Prov1/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 
-namespace Prov1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Prov1 {
+    class Program {
+        static void Main(string[] args) {
             //Introduktion
             Console.WriteLine("Beräkna din nettolön");
             Console.WriteLine("----------------------");
@@ -15,34 +12,49 @@ namespace Prov1
             Console.ForegroundColor = ConsoleColor.Yellow;
             string namn = Console.ReadLine();
             Console.ForegroundColor = ConsoleColor.White;
-        
+
             //Ange din bruttolön
-            Console.Write("Ange din bruttolön: ");
+            Console.Write("Ange din bruttolön (mellan - - - kronor): ");
             Console.ForegroundColor = ConsoleColor.Yellow;
             int bruttolön = int.Parse(Console.ReadLine());
             Console.ForegroundColor = ConsoleColor.White;
 
-            //Ange din skattesats
-            Console.Write("Ange din skattesats i procent: ");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            int skattesats = int.Parse(Console.ReadLine());
-            Console.ForegroundColor = ConsoleColor.White;
+            if (bruttolön <= -- || bruttolön >= -- ) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Din bruttolön måste vara mellan - och - kronor!");
+            } 
+            else
+            {
+                //Ange din skattesats
+                Console.Write("Ange din skattesats i procent (mellan -- och -- procent): ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                int skattesats = int.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.White;
 
-            //Nettolön beräkning
-            Console.Write($"{namn}, din nettolön blir ");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write($"{bruttolön * (100 - skattesats) / 100} kr ");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("baserat på bruttolönen ");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write($"{bruttolön} kr "); 
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("och skattesatsen ");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write($"{skattesats}%");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write(".");
-
+                if (skattesats <= -- || skattesats >= -- ) 
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Din skattesats måste vara mellan - och - kronor!");
+                } 
+                else
+                {
+                    //Nettolön beräkning
+                    Console.Write($"{namn}, din nettolön blir ");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write($"{bruttolön * (100 - skattesats) / 100} kr ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("baserat på bruttolönen ");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"{bruttolön} kr ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("och skattesatsen ");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"{skattesats}%");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(".");
+                }
+            }
             //Vänta tills programmet stängs
             Console.ReadKey();
         }
