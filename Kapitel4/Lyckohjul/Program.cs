650df607e969c8e6a3e96fb20153925b12e4b1f4
alexk732 <kron.alexander@elev.ga.ntig.se>
2021-11-04 15:54:51 +0100

    Klar med lyckohjulet

--- Kapitel4/Lyckohjul/Program.cs
+++ Kapitel4/Lyckohjul/Program.cs
@@ -14,36 +14,47 @@ namespace Lyckohjul
             int slumptal = generator.Next(1, 11);
 
             // Skapa ett program som ska fungera som ett lyckohjul
-            Console.WriteLine("Lyckohjul \n---------------");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("----------------------");
             Console.WriteLine("Välkommen till Lyckohjulet! \nDet kommer att slumpa ut ett tal mellan 1 och 10."); 
-            Console.WriteLine("Om du gissar rätt så får du ingenting! \nDu har 3 försök på dig. \n---------------");
+            Console.WriteLine("Om du gissar rätt så får du ingenting! \nDu har 3 försök på dig. \n----------------------");
 
             // När användaren kör programmet får hen välja ett tal från 1 till 10 som hen tror att lyckohjulet kommer att stanna på
             bool vann = false;
             
             for (int i = 1; i < 4; i++)
             {
-                Console.WriteLine("Välj ett tal mellan 1 och 10: "); 
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Välj ett tal mellan 1 och 10: "); 
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 int usertal = int.Parse(Console.ReadLine());
 
                 if (usertal == slumptal)
                 {
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Du gissade rätt!");
                     vann = true;
-                }
-                else
-                {
+                    Console.ReadKey();
                     
+                }
+
+                if (i < 3)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Fel! Gissa igen.");
+                    vann = false;
                 }
             }
 
             // Berätta också för användaren om hen vann eller förlorade
-            if (vann = false)
+            if (vann == false)
             {
-                
-            }
-            else
-            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Du förlorade! Det rätta talet var ");
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write($"{slumptal}");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(".");
                 Console.ReadKey();
             }
         }
--- Prov1/Program.cs
+++ Prov1/Program.cs
@@ -3,60 +3,86 @@
 namespace Prov1 {
     class Program {
         static void Main(string[] args) {
-            //Introduktion
-            Console.WriteLine("Beräkna din nettolön");
-            Console.WriteLine("----------------------");
 
-            //Skriv ditt namn
-            Console.Write("Skriv ditt namn: ");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            string namn = Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.White;
-
-            //Ange din bruttolön
-            Console.Write("Ange din bruttolön (mellan - - - kronor): ");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            int bruttolön = int.Parse(Console.ReadLine());
-            Console.ForegroundColor = ConsoleColor.White;
-
-            if (bruttolön <= -- || bruttolön >= -- ) 
+            for (var i = 1; i > 0; i++) 
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("Din bruttolön måste vara mellan - och - kronor!");
-            } 
-            else
-            {
-                //Ange din skattesats
-                Console.Write("Ange din skattesats i procent (mellan -- och -- procent): ");
+                // Introduktion
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("----------------------");
+                Console.WriteLine("Beräkna din nettolön");
+                Console.WriteLine("----------------------");
+
+                // Skriv ditt namn
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Skriv ditt namn: ");
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                int skattesats = int.Parse(Console.ReadLine());
+                string namn = Console.ReadLine();
                 Console.ForegroundColor = ConsoleColor.White;
 
-                if (skattesats <= -- || skattesats >= -- ) 
-                {
+                // Ange din bruttolön
+                Console.Write("Ange din bruttolön (mellan 10000 och 45000 kronor): ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                int bruttolön = int.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.White;
+
+                // Se ifall bruttolönen är godkänd
+                if (bruttolön < 10000 || bruttolön > 45000) {
+                    Console.WriteLine("----------------------");
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write("Din skattesats måste vara mellan - och - kronor!");
+                    Console.WriteLine($"{namn}, din bruttolön måste vara mellan 10000 och 45000 kronor!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Försök igen.");
+                    Console.ReadKey();
+                    break;
                 } 
-                else
+                else 
                 {
-                    //Nettolön beräkning
-                    Console.Write($"{namn}, din nettolön blir ");
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.Write($"{bruttolön * (100 - skattesats) / 100} kr ");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write("baserat på bruttolönen ");
+                    // Ange din skattesats
+                    Console.Write("Ange din skattesats i procent (mellan 10 och 45 procent): ");
                     Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write($"{bruttolön} kr ");
+                    int skattesats = int.Parse(Console.ReadLine());
                     Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write("och skattesatsen ");
+
+                    // Se ifall skattesaten är godkänd
+                    if (skattesats < 10 || skattesats > 45) {
+                        Console.WriteLine("----------------------");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"{namn}, din skattesats måste vara mellan 10 och 45 procent!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Försök igen.");
+                        Console.ReadKey();
+                        break;
+                    } 
+                    else 
+                    {
+                        // Nettolön beräkning
+                        Console.WriteLine("----------------------");
+                        Console.Write($"{namn}, din nettolön blir ");
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write($"{bruttolön * (100 - skattesats) / 100} kr ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("baserat på bruttolönen ");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write($"{bruttolön} kr ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("och skattesatsen ");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write($"{skattesats}%");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(".");
+                    }
+                    // Fråga om användaren vill göra en ny beräkning
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Write("\nVill du göra en ny beräkning? Skriv \"ja\": ");
                     Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write($"{skattesats}%");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write(".");
+                    string svar = Console.ReadLine();
+                    if (svar != "ja")
+                    {
+                        break;
+                    }
                 }
             }
-            //Vänta tills programmet stängs
-            Console.ReadKey();
         }
     }
 }
