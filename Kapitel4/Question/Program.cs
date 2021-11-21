bd7c1780cc84b0c817becc6f007ae78aefee307e
alexk732 <kron.alexander@elev.ga.ntig.se>
2021-11-21 15:03:51 +0100

    Jobbat med While-loopen och Uppgift 4.11

---
+++ Kapitel4/Question/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Question
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int answerCount = 5;
+
+            while (answerCount > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("--------------------");
+                Console.WriteLine("Vad heter Europas folkrikaste land?");
+                Console.WriteLine($"Du har {answerCount} försök på dig att svara rätt.");
+                Console.WriteLine("--------------------");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Ditt svar: ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                string answer = Console.ReadLine();
+
+            
+                if (answer == "Ryssland" || answer == "ryssland")
+                {
+                
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Rätt!");
+                    Console.ReadKey(true);
+                    break;
+                    
+                }
+                else
+                {  
+                    answerCount = answerCount - 1; 
+                
+                    if (answerCount > 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("Fel! Försök igen.");
+                        Console.ReadKey(true);
+                        Console.Clear();
+                        
+                    }    
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("Fel! Du har inga försök kvar.");
+                        Console.ReadKey(true);
+                    }               
+                }             
+            }     
+        }
+    }
+}
---
+++ Kapitel4/Uppgift 4.11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uppgift_4._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
